166072d391cfa0f3b118d702ed377a141410209c
gogosapiens <gogosapiens@iMac-Pro-gogosapiens.local>
2023-07-10 21:11:31 +0200

    Update

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     description='Description of your package',
     packages=find_packages(),
     install_requires=[
-        'git+https://github.com/gogosapiens/carlo',
+        'carlo @ git+https://github.com/gogosapiens/carlo'
     ],
+    dependency_links=[
+        'git+https://github.com/gogosapiens/carlo#egg=carlo'
+    ]
 )
