ff1653bc5324a8c0edb43c07350afdbe7cf8b3e5
gogosapiens <gogosapiens@iMac-Pro-gogosapiens.local>
2023-08-04 11:11:42 +0200

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sauron',
-    version='0.1.0',
+    version='0.1.1',
     description='Description of your package',
     packages=find_packages(),
     install_requires=[
