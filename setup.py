8e51f54b1c05f6e2cac5e5a01aab85036e3f6d53
gogosapiens <gogosapiens@gmail.com>
2023-08-15 17:29:33 +0200

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sauron',
-    version='0.1.1',
+    version='0.1.2',
     description='Description of your package',
     packages=find_packages(),
     install_requires=[
